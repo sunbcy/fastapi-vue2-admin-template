0f13cb190f670801403ec3bd4f45ee33a4346872
sunbcy <626813534@qq.com>
2024-11-06 04:35:16 +0800

    界面小调整;

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -16,7 +16,7 @@
               Home
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+          <a target="_blank" href="https://github.com/sunbcy/fastapi-vue2-admin-template/tree/main">
             <el-dropdown-item>Github</el-dropdown-item>
           </a>
           <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
