03bcd3cc00cff8dfaf318fed0705ec93f20a9cfe
sunbcy <626813534@qq.com>
2024-11-21 23:35:45 +0800

    测试安卓代理网络正常请求环境

---
+++ api/utils/test.py
@@ -0,0 +1,12 @@
+# ---
+# -*- coding: utf-8 -*-
+# @Software: PyCharm
+# @File: test.py
+# @Author: sunbcy
+# @Institution: SYLG University, ShenZhen, China
+# @E-mail: saintbcy@163.com
+# @Time: 11月 21, 2024 23:33
+# ---
+from utils import check_proxy
+
+print(check_proxy())
